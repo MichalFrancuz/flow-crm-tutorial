9e01c6cb2ddae6997156f7afc37a4c3d00d8b8e4
Michal <sasxcxc@icloud.com>
2023-02-08 23:34:52 +0100

    Learning of Vaadin and Springboot - first web app - adding offline logo

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -2,6 +2,7 @@ package com.example.application;
 
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @Theme(value = "flowcrmtutorial")
+@PWA(
+        name = "Vaadin CRM",
+        shortName = "CRM",
+        offlinePath = "offline.html",
+        offlineResources = {"images/icons8-organization-64.png", "images/channels4_profile.jpg"})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 public class Application implements AppShellConfigurator {
 
--- src/main/java/com/example/application/security/SecurityService.java
+++ src/main/java/com/example/application/security/SecurityService.java
@@ -3,7 +3,9 @@ package com.example.application.security;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.server.VaadinServletRequest;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SecurityService {
 
     public void logout() {
